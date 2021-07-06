5ac116a7c9b19872f3636e6d7ed5f9d885797202
baici1 <249337001@qq.com>
2021-07-06 21:47:48 +0800

    end

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,21 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-element/" : "./",
+  publicPath: './',
   devServer: {
     proxy: {
-      '/api': {
-        target: 'http://web.juhe.cn:8080/',
+      "/api": {
+        target: "http://web.juhe.cn:8080/",
         changeOrigin: true,
         ws: true,
         secure: false,
         pathRewrite: {
-          '^/api': '',
-        },
+          '^/api': ''
+        }
       },
-      // 代理的配置
     },
+    overlay: {
+      warnings: false,
+      errors: false
+    }
   },
+  lintOnSave: false
 };
