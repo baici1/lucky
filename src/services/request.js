85f32081156f3c69389a5eb393180bfa7bac9fe2
baici1 <249337001@qq.com>
2021-07-06 22:28:22 +0800

    1

--- src/services/request.js
+++ src/services/request.js
@@ -3,7 +3,7 @@ import { axiosGet } from "../libs/http";
 function getData (consName, type) {//利用promise进行异步操作顺便使用 async await 语法糖 
   return new Promise((resolve, reject) => {
     axiosGet({
-      url: `http://web.juhe.cn:8080/constellation/getAll?consName=${consName}&type=${type}`,
+      url: `/api/constellation/getAll?consName=${consName}&type=${type}`,
       success (data) {
         resolve(data);
       },
